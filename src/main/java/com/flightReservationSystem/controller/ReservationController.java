ea4b3048657ef1fab9bb0b7a2449d33e9c25505d
Faheem Shah <shahfaheem880@gmail.com>
2021-11-04 16:56:25 +0500

    Data updated

--- src/main/java/com/flightReservationSystem/controller/ReservationController.java
+++ src/main/java/com/flightReservationSystem/controller/ReservationController.java
@@ -1,5 +1,27 @@
 package com.flightReservationSystem.controller;
 
-public class ReservationController {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.flightReservationSystem.entity.Flight;
+import com.flightReservationSystem.repository.FlightRepository;
+
+@Controller
+public class ReservationController {
+	
+	@Autowired
+	FlightRepository reposit;
+
+	@RequestMapping("showCompleteReservation")
+	public String  showCompleteReservation(@RequestParam("id")int id,ModelMap modelMap) {
+		Optional<Flight> flight = reposit.findById(id)
+		modelMap.addAttribute("", flight);
+		return "completeReservation";
+	}
+	
 }
