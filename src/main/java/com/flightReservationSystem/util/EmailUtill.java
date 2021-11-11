73f16de51527c34a6a1a5c77f98e9e46e88353da
Faheem Shah <shahfaheem880@gmail.com>
2021-11-11 13:18:30 +0500

    Made new Class for mail sending

---
+++ src/main/java/com/flightReservationSystem/util/EmailUtill.java
@@ -0,0 +1,34 @@
+package com.flightReservationSystem.util;
+
+import java.io.File;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailUtill {
+
+	@Autowired
+	private JavaMailSender sender;
+	
+	public String sendItinerary(String toAddress,String filePath) {
+	MimeMessage message = sender.createMimeMessage();
+	try {
+		MimeMessageHelper messageHelper = new MimeMessageHelper(message,true);
+		messageHelper.setTo(toAddress);
+		messageHelper.setSubject("Itinerary For Your Flight");
+		messageHelper.setText("Please find your Itinerary Attached");
+		messageHelper.addAttachment("Itinerary", new File(filePath));;
+		
+	} catch (MessagingException e) {
+		e.printStackTrace();
+	}	
+	return "";
+	}
+	
+}
