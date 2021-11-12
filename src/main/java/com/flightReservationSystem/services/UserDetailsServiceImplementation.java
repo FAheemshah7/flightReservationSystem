9369a7cccdcc50127b768261e2f710f6071d3fa7
Faheem Shah <shahfaheem880@gmail.com>
2021-11-12 18:28:55 +0500

    User not found Exception Created

--- src/main/java/com/flightReservationSystem/services/UserDetailsServiceImplementation.java
+++ src/main/java/com/flightReservationSystem/services/UserDetailsServiceImplementation.java
@@ -1,15 +1,23 @@
 package com.flightReservationSystem.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.flightReservationSystem.entity.User;
+import com.flightReservationSystem.repository.UserRepository;
+
 public class UserDetailsServiceImplementation implements UserDetailsService {
 
+	
+	@Autowired 
+	UserRepository userRepository;
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+
+		User user = userRepository.findByEmail(username);
+		return new org.springframework.security.core.userdetails.UserDetails.User(user.getEmail(),user.getPass(),user.getRoles);
 	}
 
 }
