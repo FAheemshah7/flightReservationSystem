748ccb753d5429bb3cebb8d48f35c0b33cd28398
Faheem Shah <shahfaheem880@gmail.com>
2021-11-15 11:20:49 +0500

    Made SecurityServiceImplementationClass to authenticate user Login

---
+++ src/main/java/com/flightReservationSystem/services/SecurityServiceImplementation.java
@@ -0,0 +1,11 @@
+package com.flightReservationSystem.services;
+
+public class SecurityServiceImplementation implements SecurityService {
+
+	@Override
+	public boolean login(String username, String pass) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
