636f6e0035ac9729bf316567823e8e16cc6d5bbd
Faheem Shah <shahfaheem880@gmail.com>
2021-11-15 12:56:48 +0500

    Made new Package for web Security and made a new class for its configuration

---
+++ src/main/java/com/flightReservationSystem/security/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.flightReservationSystem.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		 http.authorizeRequests().antMatchers("/showRegistration","/","/index.html","registerUser","/login","/showLogin","/login/*").permitAll()
+		 .antMatchers("/admin/showAddFlights").hasAnyAuthority("ADMIN").anyRequest().authenticated().and().csrf().disable();
+		
+	}
+
+}
